8e7bd2b51090def7bfae20146bafef771cd7743f
Anuj Kumar Sharma <anujkumsharma9876@gmail.com>
2025-11-24 19:19:45 +0530

    Added things
    Signed-off-by: Anuj Kumar Sharma <anujkumsharma9876@gmail.com>

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -148,7 +148,7 @@ export const HeroSection: React.FC = () => {
               size="lg"
               variant="outline"
               className="text-lg px-10 py-5"
-              onClick={() => router.push(user ? "/editor" : "/auth/login")}
+              onClick={() => router.push("/editor")}
             >
               <Monitor className="w-5 h-5 mr-2" />
               Try Web Editor
