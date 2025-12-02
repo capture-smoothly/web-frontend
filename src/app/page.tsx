b18a4430e7db72696c40ef42c4b17809fa564933
Anuj Kumar Sharma <anujkumsharma9876@gmail.com>
2025-12-02 23:21:23 +0530

    doNE
    Signed-off-by: Anuj Kumar Sharma <anujkumsharma9876@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,9 +21,9 @@ export default function Home() {
       <main>
         <HeroSection />
         {/* <ExtensionPromotionSection /> */}
-        <HowItWorksSection />
-        <SolutionSection />
+        <HowItWorksSection /> 
         <ProblemSection />
+        <SolutionSection />
         <FeatureShowcaseSection />
         {/* <ComparisonSection /> */}
         <UseCasesSection />
